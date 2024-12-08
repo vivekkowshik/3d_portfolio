1da411960886d2d3d6686e0db681b3b66df02d50
Kowshik <121661360+vivekkowshik@users.noreply.github.com>
2024-12-08 23:19:21 +0530

    Update index.js

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,2 +1,3 @@
-import { SectionWrapper } from './path-to-hoc';
-console.log(SectionWrapper);  // Check if the import is working correctly
+import { SectionWrapper } from './hoc'; // Correct path based on your folder structure
+
+console.log(SectionWrapper);  // This will log the SectionWrapper function
