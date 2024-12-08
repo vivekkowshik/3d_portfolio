82340f316ccf9abd311565cb15c54d8bb224cb93
Kowshik <121661360+vivekkowshik@users.noreply.github.com>
2024-12-08 23:22:46 +0530

    Update index.js

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,3 +1,2 @@
-import { SectionWrapper } from './hoc'; // Correct path based on your folder structure
-
-console.log(SectionWrapper);  // This will log the SectionWrapper function
+import { SectionWrapper } from './hoc';
+export { SectionWrapper } from './SectionWrapper'; // or directly export it if defined here
